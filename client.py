3c24845904f037e795a359cfa6cf93fad64ebf09
Matan-Hodadov <59831698+Matan-Hodadov@users.noreply.github.com>
2021-08-04 16:34:29 +0300

    Update client.py

--- client.py
+++ client.py
@@ -1,25 +1,35 @@
-
+# import section
 import socket
 import time
 
+# open UDP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-server_addr = ("192.168.1.28", 12000)
 sock.settimeout(1)
 
+#server's features
+server_IP = "192.168.1.28"
+server_port = 12000
+server_addr = (server_IP, server_port)
+
+# try to send a message
 try:
     for i in range(1, 11):
         start = time.time()
         message = str(i) + " " + time.ctime(start)
         try:
+            # using sendto func to send the wanted msg
             sent = sock.sendto(message.encode(), server_addr)
+            # show that we sent the msg
             print("Sent " + message)
+            # recive msg 
             data, server = sock.recvfrom(4096)
+            # show the recived msg
             print("Received " + str(data))
             end = time.time();
-            elapsed = end - start
-            print("RTT: " + str(elapsed) + " seconds\n")
+            rtt = end - start
+            print("RTT: " + str(rtt) + " seconds\n")
         except socket.timeout:
-            print("#" + str(i) + " Requested Time out\n")
+            print("Request " + str(i) + " Timed out")
 
 finally:
     print("closing socket")
